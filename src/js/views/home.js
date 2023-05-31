7621c1034c6b958bc0fb0dad829e2792927ebe9a
tuquepantin <tuque250@gmail.com>
2023-05-31 23:12:18 +0000

    views and component

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,26 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
+import { Card } from "../component/Card.jsx";
+
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+<div className="container">
+	<div className="row">
+	    <div className="col-10">
+		    <img className="logo" src="https://www.pngmart.com/files/3/Star-Wars-Logo-PNG-Photos.png" alt="StarWars"/>
+		</div>
+		<button type="button" class="btn btn-primary col-2">Favorites</button>
 	</div>
+	<div>
+		<h2>Characters</h2>
+		<div className="scroll">
+			<Card />
+		</div>
+		<h2>Planets</h2>
+		<div className="scroll">
+			<Card />
+		</div>
+
+	</div>
+</div>
 );
